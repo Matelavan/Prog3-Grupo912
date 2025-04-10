36d5af226fbff794afc33aadb7c19fd9aa812511
Matelavan <lavandeiram@udesa.edu.ar>
2025-04-10 12:58:13 -0300

    agregue un poco mas del resultado de busqueda

--- src/components/FiltroPelis/FiltroPelis.js
+++ src/components/FiltroPelis/FiltroPelis.js
@@ -1,10 +1,26 @@
 import React, {Component} from 'react'
 
 class FiltroPelis extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            valorInput:'',
+        }
+    }
+    manejarSubmit(evento){
+        evento.preventDefault();
+    }
+    controlarForm(evento){
+        console.log('esta es la pelin que llega', evento)
+        this.setState(
+            {valorInput: evento.target.value},
+            () => this.props.filtro (this.state.valorInput)
+            )
+    }
     render(){
         return(
-            <form>
-                <input/>
+            <form onSubmit={(evento) => this.manejarSubmit(evento)}>
+                <input onChange={(evento)=> this.controlarForm(evento)} value={this.state.valorInput}/>
             </form>
         )
     }
