c505009d9992ead347b1cde7b6272c530dc950cd
estanim <emiedziak@udesa.edu.ar>
2025-04-12 16:15:52 -0300

    agregue pagina favoritos

--- src/screens/Favoritos.js
+++ src/screens/Favoritos.js
@@ -1,11 +1,42 @@
 import React, { Component } from "react";
+import MovieCard from './../components/MovieCard/MovieCard';
 
-function Favoritos() {
-    return(
 
-        <h1>Aqui estan tus peliculas favoritas!!!</h1>
-    )
-
+class Favoritos extends Component {
+    constructor(){
+        super();
+        this.state = {
+            peliculasFavoritas: []
+        }
+    }
+    componentDidMount(){
+        let favoritos = [];
+        let webstorage = localStorage.getItem("favoritos");
+        if (webstorage!== null) {
+            favoritos = JSON.parse(webstorage)
+            let peliculas = [];
+            favoritos.forEach(unid => {
+                fetch(`https://api.themoviedb.org/3/movie/${unid}?api_key=30fb07c3e57fc8656acc83104dff9754&language=es-ES`)
+                .then(res => res.json())
+                .then(data => {peliculas.push(data)
+                    this.setState({
+                        peliculasFavoritas: peliculas
+                    })
+                })
+                .catch("error")
+            });
+        }
+    }
+    render(){
+        return(<React.Fragment>
+            <h2>Peliculas Favoritas</h2>
+            <div className="peliculas">
+                                {this.state.peliculasFavoritas.map(peli => (
+                                    <MovieCard key={peli.id} data={peli} />
+                                ))}
+                            </div>
+        </React.Fragment>)
+    }
 }
 
 
